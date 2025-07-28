d5a8b6c1a1272384a417c91225e579195ae0fe48
automatedalgo <sam@automatedalgo.com>
2025-07-28 22:51:09 +0100

    migrate away from using embedded GX server

--- src/examples/standalone/apex-demo-simple-market-maker.cpp
+++ src/examples/standalone/apex-demo-simple-market-maker.cpp
@@ -16,22 +16,24 @@ with Apex. If not, see <https://www.gnu.org/licenses/>.
 */
 
 #include <apex/core/Bot.hpp>
-#include <apex/core/GatewayService.hpp>
+#include <apex/core/Logger.hpp>
+#include <apex/core/MarketDataService.hpp>
+#include <apex/core/OrderRouterService.hpp>
 #include <apex/core/Strategy.hpp>
-#include <apex/gx/GxServer.hpp>
 
+#include <iostream>
 
 class SimpleMarketMakerBot : public apex::Bot
 {
 
   struct Config {
-    double order_usd = 10;
+    double order_usd = 50;  // value of each order, in USD
 
     // When placing an order we choose a price that is based on a distance from
     // the best limit price.
-    double entry_price_distance_bps = 20;
+    double entry_price_distance_bps = 50;
 
-    double price_threshold_bps = 10;
+    double price_threshold_bps = 5;
   };
   const Config params;
 
@@ -44,19 +46,35 @@ public:
   {
     // Every 1 second run the Bot logic, which is just an order if we already
     // have not, but if we have sent one, then cancel it.
-    if (_order)
-      _manage_existing_order();
-    else
-      _create_and_send_order();
+
+    if (_make_buy)
+    {
+      if (_order_buy)
+        _manage_existing_order(_order_buy, _price_bounds_buy);
+      else
+        _create_and_send_order(apex::Side::buy, _order_buy, _price_bounds_buy);
+    }
+
+    if (_make_sell)
+    {
+      if (_order_sell)
+        _manage_existing_order(_order_sell, _price_bounds_sell);
+      else
+        _create_and_send_order(apex::Side::sell, _order_sell, _price_bounds_sell);
+    }
   }
 
-  virtual void on_order_fill(apex::Order&) {
-    auto pos = this->position();
-  }
 
 private:
 
-  void _create_and_send_order()
+  struct PriceBounds {
+    double lower;
+    double upper;
+  };
+
+  void _create_and_send_order(apex::Side side,
+                              std::shared_ptr<apex::Order>& order,
+                              PriceBounds & px_bounds)
   {
     if (!market_data_ok() || !has_fx_rate()) {
       LOG_WARN(ticker() << ": waiting for market data");
@@ -66,75 +84,88 @@ private:
     if (is_stopping())
       return;
 
-    double price = round_price_passive(last_price() * (1.0 - params.entry_price_distance_bps/10000), apex::Side::buy);
+    int iside = (side == apex::Side::buy)? 1: -1;
+
+    double price = round_price_passive(
+      last_price() * (1.0 - iside*params.entry_price_distance_bps/10000),
+      side);
+
+    // don't send order if calculated order is zero
+    if (apex::dbl_is_zero(price)) {
+      LOG_WARN("cannot send, calculated order price is " << price);
+      return;
+    }
 
     // determine the market price lower & upper bounds that will prompt a
     // replace of the order
-    _replace_price.lower = last_price() * (1.0 - params.price_threshold_bps/10000);
-    _replace_price.upper = last_price() * (1.0 + params.price_threshold_bps/10000);
+    px_bounds.lower = last_price() * (1.0 - params.price_threshold_bps/10000);
+    px_bounds.upper = last_price() * (1.0 + params.price_threshold_bps/10000);
 
     // size the order quantity, based on target price and value
     double qty = round_size(params.order_usd / (price * fx_rate()));
 
     // don't send order if calculated order qty is zero
-    if (qty == 0)
+    if (apex::dbl_is_zero(qty) == 0) {
+      LOG_INFO("order qty is zero, not sending for side " << side);
       return;
+    }
 
     // construct an order object (this does not cause it to be sent)
-    _order = create_order(apex::Side::buy, qty, price, apex::TimeInForce::gtc);
+    order = create_order(side, qty, price, apex::TimeInForce::gtc);
 
     // send order to the exchange (this is an asynchronous operation)
-    _order->send();
+    order->send();
   }
 
-  void _manage_existing_order()
+  void _manage_existing_order(std::shared_ptr<apex::Order>& order,
+                              PriceBounds& px_bounds)
   {
+    if (!order->is_closed_or_canceling()) {
 
-    if (!_order->is_closed_or_canceling()) {
-
-      if ((last_price() > _replace_price.upper) || last_price() < _replace_price.lower) {
-        // LOG_INFO("last price out of range, cancelling order");
-        _order->cancel();
+      if ((last_price() > px_bounds.upper) || last_price() < px_bounds.lower) {
+        LOG_INFO("last price out of range, cancelling order");
+        order->cancel();
       }
 
       // The order is still 'live', so here we will manage it.  Our only
       // management logic is to cancel the order if it's been alive for too
       // long.
 
-      if (_order->duration_live() > std::chrono::seconds(20)) {
-        //_order->cancel();
+      if (order->duration_live() > std::chrono::seconds(20)) {
+        order->cancel();
       }
     }
     else {
-      if (_order->is_closed()) {
-        _order.reset();
+      if (order->is_closed()) {
+        order.reset();
       }
     }
   }
 
-  std::shared_ptr<apex::Order> _order;
+  bool _make_buy = true;
+  bool _make_sell = true;
 
-  struct PriceBounds {
-    double lower;
-    double upper;
-  };
-  PriceBounds _replace_price;
+  std::shared_ptr<apex::Order> _order_buy;
+  std::shared_ptr<apex::Order> _order_sell;
+
+  PriceBounds _price_bounds_buy;
+  PriceBounds _price_bounds_sell;
 };
+
+
 
 int main()
 {
   try {
-    // This demo can run in either backtest, paper, or live run-mode.  Use this
-    // control to decide.
-    // THIS
+    // Configure thie demo can run in either backtest, paper, or live trading.
+    // Hoever to run in back-test mode will require captured market data.
     auto run_mode = apex::RunMode::paper;
 
-    // create core engine, configured for paper or live trading
     std::unique_ptr<apex::Services> services;
 
-    // ----- Framework setup -----
-
-    std::unique_ptr<apex::GxServer> embedded_gateway;
+    // ------------------------------------------------------------
+    // FRAMEWORK SETUP
+    // ------------------------------------------------------------
 
     if (run_mode == apex::RunMode::backtest) {
       // backtest time range
@@ -144,33 +175,32 @@ int main()
       // create core engine, configured for backtest
       services = apex::Services::create(run_mode, {from, upto});
     } else {
+      // create core engine, configured for realtime (prod / paper)
       services = apex::Services::create(run_mode);
 
-      // create an embedded exchange gateway server
-      embedded_gateway = std::make_unique<apex::GxServer>(services->realtime_evloop(),
-                                                          run_mode);
+      // add feed handlers
+      apex::FeedConfig feed_config;
+      feed_config.type = "BinanceUsdFut";
+      services->market_data_service()->add_feed(feed_config,
+                                                {"binance_usdfut"});
 
-      // add Binance to gateway (set user's private API key for live trading)
-      apex::BinanceSession::Params params;
-      //params.api_key_file = apex::user_home_dir()/".apex/binance_key.json";
-      embedded_gateway->add_venue(params);
-
-      // start the gateway, it will listen for connections from the strategy
-      embedded_gateway->start();
-
-      // add a route from core engine to the gateway server
-      services->gateway_service()->set_default_gateway(embedded_gateway->get_listen_port());
+      // add line handlers
+      apex::OrderRouterConfig binance_config;
+      binance_config.api_key_file = apex::expand("~/.secrets/binance_key.json");
+      services->order_router_service()->add_binance_usdfut(binance_config);
     }
 
-    // ----- Strategy -----
+    // ------------------------------------------------------------
+    //  STRATEGY
+    // ------------------------------------------------------------
 
     // create a Strategy object, which is a container for individual bots
-    apex::Strategy strategy(services.get(), "DEM02");
+    apex::Strategy strategy(services.get(), "SMM02");
 
     // add a bot, which is responsible for trading a single name
     strategy.create_bot<SimpleMarketMakerBot>(apex::InstrumentQuery(
-                                                "BTCUSDT",
-                                                apex::ExchangeId::binance));
+                                                "DOGEUSDT",
+                                                apex::ExchangeId::binance_usdfut));
 
     // initialise all bots, so they can begin trading
     strategy.init_bots();
