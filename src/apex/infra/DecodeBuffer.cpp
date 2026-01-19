4d9db5d3f50c1367945de39303e582826797323c
automatedalgo <sam@automatedalgo.com>
2026-01-19 08:10:13 +0000

    support padding on the decode buffer

--- src/apex/infra/DecodeBuffer.cpp
+++ src/apex/infra/DecodeBuffer.cpp
@@ -24,24 +24,13 @@ namespace apex
 {
 
 
-DecodeBuffer::DecodeBuffer(size_t initial_size, size_t max_size)
-  : _mem(initial_size), _max_size(max_size), _bytes_avail(0)
+DecodeBuffer::DecodeBuffer(size_t size, size_t max_size, size_t padding)
+  : _bytes_avail(0),
+    _mem(size+padding, 0),
+    _max_size(max_size),
+    _pad_size(padding)
 {
-}
-
-
-void DecodeBuffer::update_max_size(size_t new_max)
-{
-  if (new_max == _max_size)
-    return;
-
-  if ((new_max < _max_size) && (new_max < _mem.size()))
-    throw std::runtime_error("unable to reduce DecodeBuffer max size");
-
-  _max_size = new_max;
-
-  // Note: don't perform any actual DecodeBuffer modification, since it would
-  // invalidate any read_pointer
+  reset_padding();
 }
 
 
@@ -54,26 +43,37 @@ size_t DecodeBuffer::consume(const char* src, size_t len)
   if (len && consume_len == 0)
     throw std::runtime_error("DecodeBuffer full, cannot consume data");
 
-  memcpy(_mem.data() + _bytes_avail, src, consume_len);
+  memcpy(data() + _bytes_avail, src, consume_len);
   _bytes_avail += consume_len;
 
   return consume_len;
 }
 
-void DecodeBuffer::grow_by(size_t len)
+
+void DecodeBuffer::grow_by(size_t extra_len)
 {
-  size_t grow_max = _max_size - _mem.size();
-  size_t grow_size = (std::min)(grow_max, len);
-  if (grow_size)
-    _mem.resize(_mem.size() + grow_size);
+  size_t extra_max = _max_size - buffer_size();
+  size_t grow_size = std::min(extra_max, extra_len);
+  if (grow_size) {
+    _mem.resize(_mem.size() + _pad_size + grow_size);
+     reset_padding();
+  }
 }
 
 
 void DecodeBuffer::discard(const read_pointer& rd)
 {
   _bytes_avail = rd.avail();
-  if (rd.ptr() != _mem.data() && rd.avail())
-    memmove(_mem.data(), rd.ptr(), rd.avail());
+  if (rd.ptr() != data() && rd.avail())
+    memmove(data(), rd.ptr(), rd.avail());
 }
+
+
+void DecodeBuffer::reset_padding()
+{
+  if (_pad_size)
+    memset(data() + buffer_size(), ' ', _pad_size);
+}
+
 
 } // namespace apex
